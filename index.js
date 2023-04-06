ac1669d14432900df96b244294b773569a7ecfa9
Arushi Mishra <35418397+aru5hi@users.noreply.github.com>
2023-04-06 22:58:16 +0530

    update resume button function

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.getElementById('top').addEventListener('click', function(event){
 });
 
 function resumeRedirect(){
-  window.open("https://drive.google.com/file/d/1-PS3jMn4zYBBnF8t53ldKRMSw7F_vkwc/view?usp=sharing", '_blank');
+  window.open("https://linkedin.com/in/arushi-mishra-220082176", '_blank');
 }
 
 (function() {
